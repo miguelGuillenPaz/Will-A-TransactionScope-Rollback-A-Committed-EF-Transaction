7be78451097f18db6719fbddfe8de92701d89c1f
John MacIntyre <john.macintyre@jatensystems.com>
2014-01-29 14:52:49 -0500

    Added TransactionScope & committed that it does rollback

--- TransactionTest/TransactionTest/Program.cs
+++ TransactionTest/TransactionTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace TransactionTest
 {
@@ -10,13 +11,17 @@ namespace TransactionTest
     {
         static void Main(string[] args)
         {
-            var m = new Movie { Title = "Rocky" };
-            using (var ctx = new TransactionTestDbContext())
-            using(var trans = ctx.Database.BeginTransaction())
+            var m = new Movie { Title = "Rocky V" };
+            using (var transScope = new TransactionScope())
             {
-                ctx.Movies.Add(m);
-                ctx.SaveChanges();
-                trans.Commit();
+                using (var ctx = new TransactionTestDbContext())
+                using (var trans = ctx.Database.BeginTransaction())
+                {
+                    ctx.Movies.Add(m);
+                    ctx.SaveChanges();
+                    trans.Commit();
+                }
+                // transScope.Complete(); // no commit is a rollback
             }
         }
     }
